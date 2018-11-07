96d2b924e40db9224b7de3a23370070f8492d6ff
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-11-07 19:16:16 +0800

    增加基本消息类型的结构声明。

--- plugin/message/proptype.js
+++ plugin/message/proptype.js
@@ -21,7 +21,49 @@ export const TextMessage = {
 export const ImageMessage = {
     ...Types.BasicMessage,
     data: PropTypes.shape({
-        thumbnailPath: PropTypes.string,
+        thumbnailLocalPath: PropTypes.string,
+        thumbnailRemotePath: PropTypes.string,
+        localPath: PropTypes.string,
         remotePath: PropTypes.string,
+        size: PropTypes.shape({
+            width: PropTypes.number,
+            height: PropTypes.number,
+        }),
+    }),
+};
+
+export const VoiceMessage = {
+    ...Types.BasicMessage,
+    data: PropTypes.shape({
+        localPath: PropTypes.string,
+        remotePath: PropTypes.string,
+        duration: PropTypes.number,
+    }),
+};
+
+export const LocationMessage = {
+    ...Types.BasicMessage,
+    data: PropTypes.shape({
+        latitude: PropTypes.number,
+        longitude: PropTypes.number,
+        address: PropTypes.string,
+        name: PropTypes.string,
+    }),
+};
+
+export const VideoMessage = {
+    ...Types.BasicMessage,
+    data: PropTypes.shape({
+        localPath: PropTypes.string,
+        remotePath: PropTypes.string,
+    }),
+};
+
+export const FileMessage = {
+    ...Types.BasicMessage,
+    data: PropTypes.shape({
+        localPath: PropTypes.string,
+        remotePath: PropTypes.string,
+        size: PropTypes.number,
     }),
 };
