413aaf335a49e89a7b732e685a5b3f65401a1a81
Xiaosong Gao <gaoxiaosong06@gmail.com>
2019-02-04 13:55:58 +0800

    添加文本消息框的表情展示。

--- plugin/message/display/TextBubble.js
+++ plugin/message/display/TextBubble.js
@@ -1,24 +1,68 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Image, StyleSheet, View, Text, Platform } from 'react-native';
 import { DisplayProps, TextMessage } from '../proptype';
+import * as IMStandard from '../../../src';
 
 export default class extends React.PureComponent {
     static propTypes = DisplayProps(TextMessage);
+
+    constructor(props) {
+        super(props);
+        const {message: {data: {text}}} = props;
+        this.textArr = [];
+        let str = '';
+        if (text) {
+            for (let i = 0; i < text.length; i++) {
+                const char = text.charAt(i);
+                if (char === '[') {
+                    this.textArr.push(str);
+                    str = char;
+                } else if (char === ']') {
+                    str = str + char;
+                    this.textArr.push(str);
+                    str = '';
+                } else {
+                    str = str + char;
+                }
+            }
+        }
+        if (str.length > 0) {
+            this.textArr.push(str);
+        }
+    }
 
     componentDidMount() {
         this.props.enableBubble && this.props.enableBubble(true);
     }
     
     render() {
-        const {message: {data: {text}}} = this.props;
-        // TODO 表情和URL、电话支持，居中对齐
+        // TODO URL、电话支持，居中对齐
         return (
             <View style={styles.view}>
-                <Text style={styles.text}>
-                    {text}
-                </Text>
+                {this.textArr.map(this._renderItem.bind(this))}
             </View>
         );
+    }
+
+    _renderItem(text, index) {
+        const isEmoji = text[0] === '[' && text[text.length - 1] === ']';
+        const emojiImage = IMStandard.Delegate.model.Emoji.getEmoji(text);
+        if (isEmoji && emojiImage) {
+            return (
+                <Image
+                    key={index}
+                    source={emojiImage}
+                    style={styles.image}
+                    resizeMode={'center'}
+                />
+            );
+        } else {
+            return (
+                <Text key={index} style={styles.text}>
+                    {text}
+                </Text>
+            );
+        }
     }
 }
 
@@ -30,9 +74,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
+        flexWrap: 'wrap',
     },
     text: {
         fontSize: 16,
+        backgroundColor: 'transparent',
+        ...Platform.select({
+            android: {textAlignVertical: 'center'},
+            ios: {lineHeight: 18},
+        }),
+        overflow: 'hidden',
+    },
+    image: {
+        width: 18,
+        height: 18,
+        margin: 1,
         backgroundColor: 'transparent',
     },
 });
