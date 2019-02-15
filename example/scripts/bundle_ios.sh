48c86f4a8f93e1c443e967c56cfa91145148a77b
Xiaosong Gao <gaoxiaosong06@gmail.com>
2019-02-16 01:48:23 +0800

    Fix：iOS样例工程的Bug。

--- example/scripts/bundle_ios.sh
+++ example/scripts/bundle_ios.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 
 mainBundleName="main.jsbundle"
-destPath="./ios/Example/Resource/"
+destPath="./ios/Example/"
 mainBundlePath=${destPath}${mainBundleName}
 jsAssetsPath=${destPath}
 
 rm -rf ${jsAssetsPath}assets
 echo "Generating ${mainBundleName}..."
 node node_modules/react-native/local-cli/cli.js bundle \
-    --entry-file index.ios.js \
+    --entry-file index.js \
     --bundle-output $mainBundlePath \
     --platform ios \
     --assets-dest $jsAssetsPath \
