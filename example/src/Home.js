4642fb757fbc5e929b74d347b11c1772ae41a784
Xiaosong Gao <gaoxiaosong06@gmail.com>
2019-02-16 05:11:27 +0800

    优化样例工程首页UI展示。

--- example/src/Home.js
+++ example/src/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, ScrollView } from 'react-native';
 import { Client } from 'react-native-im-easemob';
 import { IMStandard } from 'react-native-im';
+import i18n from 'i18n-js';
+import ArrowImage from '@hecom/image-arrow';
 import * as Constant from './Constant';
 
 export default class extends React.PureComponent {
-    static navigationOptions = {
-        title: '首页',
+    static navigationOptions = function () {
+        return {
+            title: i18n.t('HomeTitle'),
+        };
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    
     componentDidMount() {
         if (Constant.Account && Constant.Password) {
             IMStandard.Delegate.user.getMine = () => ({userId: Constant.Account});
@@ -19,25 +28,49 @@ export default class extends React.PureComponent {
 
     render() {
         return (
-            <View style={styles.view}>
-                <TouchableOpacity onPress={this._loginSucceed}>
-                    <Text>
-                        {'Login'}
-                    </Text>
-                </TouchableOpacity>
-            </View>
+            <ScrollView style={styles.view}>
+                {this._renderLine(i18n.t('Register'), this._register.bind(this))}
+                {this._renderLine(i18n.t('Login'), this._login.bind(this))}
+            </ScrollView>
         );
     }
 
-    _loginSucceed = () => {
+    _renderLine(label, onPress) {
+        return (
+            <TouchableOpacity onPress={onPress}>
+                <View style={styles.line}>
+                    <Text style={styles.text}>
+                        {label}
+                    </Text>
+                    <ArrowImage />
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    _register() {
+    }
+
+    _login() {
         this.props.navigation.navigate(IMStandard.PageKeys.ChatList);
-    };
+    }
 }
 
 const styles = StyleSheet.create({
     view: {
         flex: 1,
+    },
+    line: {
+        height: 60,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        justifyContent: 'center',
+        paddingHorizontal: 16,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#dddddd',
+    },
+    text: {
+        fontSize: 16,
+        color: '#333333',
     },
 });
