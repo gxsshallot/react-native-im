12115d292b5b5607d09b0e2a8c40e62b1e194e49
白宇东 <summer88123@163.com>
2019-08-28 10:13:51 +0800

    提交遗漏文件

---
+++ standard/render/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import Badge from '@hecom/badge';
+import {getSafeAreaInset} from 'react-native-pure-navigation-bar';
+
+function renderBadge(avoid, count) {
+    const {width} = Dimensions.get('window');
+    const inset = getSafeAreaInset();
+    const style = avoid ? {
+        top: 5,
+        right: width - 70 - inset.left + 5,
+    } : {
+        top: 0,
+        right: width - 70 - inset.left,
+    };
+    return (
+        <Badge
+            count={avoid ? null : count}
+            maxCount={99}
+            radius={avoid ? 3 : 8}
+            outSpace={2}
+            style={[styles.badge, style]}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    badge: {position: 'absolute', zIndex: 1}
+});
+
+
+export {
+    renderBadge
+}
